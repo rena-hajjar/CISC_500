53b1a15f9f8f442fcbd1a0e0583a184d224727a4
Rena <renashajjar@gmail.com>
2025-11-12 18:59:01 -0500

    save model as ensemble pytorch

--- blank.py
+++ blank.py
@@ -1,60 +1,113 @@
+import torch
+from typing import Tuple, Union
+
+import torch
+from batchgenerators.utilities.file_and_folder_operations import load_json, join
+
+import nnunetv2
+from nnunetv2.utilities.find_class_by_name import recursive_find_python_class
+from nnunetv2.utilities.label_handling.label_handling import determine_num_input_channels
+from nnunetv2.utilities.plans_handling.plans_handler import PlansManager
+
+import logging
+import argparse
 import os
-import numpy as np
-from PIL import Image
-import nibabel as nib
 
-# # Path to your folder
-# input_folder = r"C:\Users\Rena\Documents\BCS Cavity Scanning\USFrames2"
-# output_folder = r"C:\Users\Rena\Documents\BCS Cavity Scanning\USFrames2ForInference"
+def parseArgs():
+    # Parse command line arguments
 
-# os.makedirs(output_folder, exist_ok=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output_dir", type=str)
+    parser.add_argument("--in_channels", type=int, default=1)
+    args = parser.parse_args()
+    return args
 
-# for filename in sorted(os.listdir(input_folder)):
-#     if filename.endswith(".png") and filename.startswith("frame_"):
-#         # Extract original number and remove leading zeros
-#         num = int(filename.split('_')[1].split('.')[0])
-#         new_name = f"frame_{num:03d}_0000.nii.gz"
-        
-#         # Load PNG and convert to numpy array
-#         img_path = os.path.join(input_folder, filename)
-#         img = Image.open(img_path).convert("L")  # convert to grayscale
-#         data = np.array(img)
-        
-#         # Add extra dimension for NIfTI (3D)
-#         data = data[np.newaxis, :, :]  # shape: (1, H, W)
-        
-#         # Create NIfTI image
-#         nii_img = nib.Nifti1Image(data, affine=np.eye(4))
-#         nib.save(nii_img, os.path.join(output_folder, new_name))
 
-# print("Conversion done!")
+class nnUNetPredictor(object):
+    def __init__(self,
+                 tile_step_size: float = 0.5,
+                 use_gaussian: bool = True,
+                 use_mirroring: bool = True,
+                 perform_everything_on_device: bool = True,
+                 device: torch.device = torch.device('cuda'),
+                 verbose: bool = False,
+                 verbose_preprocessing: bool = False,
+                 allow_tqdm: bool = True):
+        self.verbose = verbose
+        self.verbose_preprocessing = verbose_preprocessing
+        self.allow_tqdm = allow_tqdm
 
-import os
-import nibabel as nib
-import numpy as np
-from PIL import Image
+        self.plans_manager, self.configuration_manager, self.list_of_parameters, self.network, self.dataset_json, \
+        self.trainer_name, self.allowed_mirroring_axes, self.label_manager = None, None, None, None, None, None, None, None
 
-pred_folder = r"C:\Users\Rena\Documents\BCS Cavity Scanning\Frames2InferenceResults"      # NIfTI prediction files
-out_folder = r"C:\Users\Rena\Documents\BCS Cavity Scanning\USFrames2Segmentations"   # Output PNG folder
+        self.tile_step_size = tile_step_size
+        self.use_gaussian = use_gaussian
+        self.use_mirroring = use_mirroring
+        if device.type == 'cuda':
+            torch.backends.cudnn.benchmark = True
+        else:
+            print(f'perform_everything_on_device=True is only supported for cuda devices! Setting this to False')
+            perform_everything_on_device = False
+        self.device = device
+        self.perform_everything_on_device = perform_everything_on_device
 
-os.makedirs(out_folder, exist_ok=True)
 
-for f in sorted(os.listdir(pred_folder)):
-    if f.endswith(".nii.gz"):
-        nii_path = os.path.join(pred_folder, f)
-        img = nib.load(nii_path)
-        data = img.get_fdata()   # shape: (1, H, W) or (H, W)
+def initialize_from_trained_model_folder(model_training_output_dir: str,
+                                             use_folds: Union[Tuple[Union[int, str]], None],
+                                             checkpoint_name: str = 'checkpoint_final.pth'):
+        """
+        This is used when making predictions with a trained model
+        """
+        args = argparse()
 
-        # If 3D or channel-first, squeeze extra dimensions
-        if data.ndim == 3:
-            data = data[0]       # take first channel
+        if use_folds is None:
+            use_folds = nnUNetPredictor.auto_detect_available_folds(model_training_output_dir, checkpoint_name)
 
-        # Convert to binary mask: 0 = background, 255 = foreground
-        # Assumes 0 = background, anything else = foreground
-        binary_mask = np.where(data > 0, 255, 0).astype(np.uint8)
+        dataset_json = load_json(join(model_training_output_dir, 'dataset.json'))
+        plans = load_json(join(model_training_output_dir, 'plans.json'))
+        plans_manager = PlansManager(plans)
 
-        # Save as PNG
-        out_path = os.path.join(out_folder, f.replace(".nii.gz", ".png"))
-        Image.fromarray(binary_mask).save(out_path)
+        if isinstance(use_folds, str):
+            use_folds = [use_folds]
 
-print(f"Converted {len(os.listdir(out_folder))} NIfTI masks to 0/255 PNGs!")
+        parameters = []
+        for i, f in enumerate(use_folds):
+            f = int(f) if f != 'all' else f
+            checkpoint = torch.load(join(model_training_output_dir, f'fold_{f}', checkpoint_name),
+                                    map_location=torch.device('cpu'), weights_only=False)
+            if i == 0:
+                trainer_name = checkpoint['trainer_name']
+                configuration_name = checkpoint['init_args']['configuration']
+            parameters.append(checkpoint['network_weights'])
+
+        configuration_manager = plans_manager.get_configuration(configuration_name)
+        # restore network
+        num_input_channels = determine_num_input_channels(plans_manager, configuration_manager, dataset_json)
+        trainer_class = recursive_find_python_class(join(nnunetv2.__path__[0], "training", "nnUNetTrainer"),
+                                                    trainer_name, 'nnunetv2.training.nnUNetTrainer')
+        if trainer_class is None:
+            raise RuntimeError(f'Unable to locate trainer class {trainer_name} in nnunetv2.training.nnUNetTrainer. '
+                               f'Please place it there (in any .py file)!')
+        network = trainer_class.build_network_architecture(
+            configuration_manager.network_arch_class_name,
+            configuration_manager.network_arch_init_kwargs,
+            configuration_manager.network_arch_init_kwargs_req_import,
+            num_input_channels,
+            plans_manager.get_label_manager(dataset_json).num_segmentation_heads,
+            enable_deep_supervision=False
+        )
+
+        # initialize network with first set of parameters, also see https://github.com/MIC-DKFZ/nnUNet/issues/2520
+        network.load_state_dict(parameters[0])
+
+        model = model.to("cpu")
+        model.eval()
+        example_input = torch.rand(1, args.in_channels, 128, 128)
+        traced_script_module = torch.jit.trace(model, example_input)
+        traced_script_module.save(os.path.join(args.output_dir, "model_traced.pt"))
+        logging.info(f"Traced model saved to {os.path.join(args.output_dir, 'model_traced.pt')}.")
+
+
+if __name__=="__main__":
+    model_training_dir = r"C:\Users\Rena\Documents\YEAR4\CISC500\CISC_500\nnUNet_Data\nnUNet_results\Dataset555\nnUNetTrainer__nnUNetPlans__2d"
+    initialize_from_trained_model_folder(model_training_output_dir=model_training_dir)
